98c0eef04176943ad41c06d48b00998e6f1e6107
dperalta <peralta.dani@gmail.com>
2018-09-27 02:18:19 -0300

    Agrego referencias a Sugar y TicketDao

---
+++ app/src/main/java/com/grupoprominente/android/viaticket/application/ViaTicketApplication.java
@@ -0,0 +1,16 @@
+package com.grupoprominente.android.viaticket.application;
+
+import android.app.Application;
+
+import com.orm.SugarContext;
+
+public class ViaTicketApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        SugarContext.init(this);
+    }
+}
